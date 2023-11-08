056a205b8871219bf134cf21a577fb2956901a6d
A.Rivera <a.rivera@cip-ws-108.cip.physik.uni-muenchen.de>
2023-11-08 21:47:47 +0100

    new truncation and symmetric jumping rate

---
+++ SmartTASEP/gym-environments/gym_environments/envs/__init__.py
@@ -0,0 +1,3 @@
+from gym_environments.envs.Lattice import LatticeEnv
+from gym_environments.envs.GridWorld import GridWorldEnv
+from gym_environments.envs.FrozenLake import FrozenLakeEnv
---
+++ SmartTASEP/gym-environments/gym_environments/wrappers/__init__.py
@@ -0,0 +1,4 @@
+from gym_environments.wrappers.clip_reward import ClipReward
+from gym_environments.wrappers.discrete_actions import DiscreteActions
+from gym_environments.wrappers.reacher_weighted_reward import ReacherRewardWrapper
+from gym_environments.wrappers.relative_position import RelativePosition
